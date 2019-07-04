568f321d567093cfb90357a6fc28008717ea2b17
damianmatusik96 <39829963+damianmatusik96@users.noreply.github.com>
2019-07-04 23:09:36 +0200

    Revert "Test"

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     data_cluster.choose_best_cluster(2, 8)
     profile_predictor = ProfilePredictor(data_handler_3.raw_data_pr, data_cluster)
+
     profile_predictor.get_best_profile()
     data_cluster_3.choose_best_cluster(2, 8)
     plt.show()
